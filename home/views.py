0d7d2fdaed43dc4d991604bd1da8ee2265e90cf0
leo-jp <peralta.ljz.it@gmail.com>
2022-01-05 22:08:35 +0800

    v.0.13 - User Authentication

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from datetime import date, datetime
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'home/index.html', {'today': datetime.today()})
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
